8ab3680e61427ce65dd010b6dff0beb787e52aca
ユムル PC <yumuru@wlan-napt-003.u-aizu.ac.jp>
2019-07-29 11:20:07 +0900

    MusicPlayer, MusicChart, GameScore and Manager

--- Assets/Scripts/PlayerAction/ConductStick.cs
+++ Assets/Scripts/PlayerAction/ConductStick.cs
@@ -9,17 +9,25 @@ public class ConductStick : MonoBehaviour {
     GamePlayerMain m_playerMain;
     VRStick m_stick;
     public float m_speedThreshold;
+    public bool isDebug;
 
     void Start() {
         m_playerMain = GetComponentInParent<GamePlayerMain>();
         m_stick = GetComponentInParent<VRStick>();
-        ConductAction();
     }
 
-    void ConductAction() {
+    void SetConductAction() {
+        var onConduct = new Subject<Unit>();
         m_stick.UpdateAsObservable()
             .Where(_ => m_stick.device.velocity.sqrMagnitude >= Mathf.Pow(m_speedThreshold, 2f))
-            .Subscribe(_ => {
+            .Subscribe(onConduct);
+        if (isDebug) {
+            this.UpdateAsObservable()
+                .Where(_ => isDebug)
+                .Where(_ => Input.GetKeyDown(KeyCode.C))
+                .Subscribe(onConduct);
+        }
+        onConduct.Subscribe(_ => {
                 var timing = new Timing(Music.Just);
                 if (TimingManager.CouldConduct(timing)) {
                     print("conduct!");
---
+++ Assets/Scripts/System/TestMusic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+public class TestMusic : MonoBehaviour {
+    [SerializeField]
+    AudioSource m_nonMusicSource, m_musicSource;
+    MusicPlayer.Clip[] m_clips;
+    MusicPlayer m_player;
+    void Awake() {
+        m_player = new MusicPlayer(m_nonMusicSource, m_musicSource, m_clips);
+        GameManager.CurrentPlayer = m_player;
+    }
+    void Start() {
+        var chart = new MusicChart();
+        chart.AddGhost(new Timing(3,0,0), 0, 1);
+    }
+}
