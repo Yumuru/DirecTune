e81a7ff96831c508af03ea407acd58ea564ded07
K-197 <kota.hoshino.0418@outlook.jp>
2019-11-10 16:06:41 +0900

    エフェクトとState

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,19 +14,34 @@ public class GameManager : MonoBehaviour {
 	public MusicManager m_musicManager;
 
 	public Subject<Unit> m_onPlay = new Subject<Unit>();
+    public enum State
+    {
+        Start,
+        Play,
+        End
+    }
+    public State m_correntState = State.Start;
 
 	private void Awake() {
 		Ins = this;
 	}
 
 	private void Start() {
+        m_onPlay.Subscribe(_ => {
+            Music.Play("Music");
+            State m_correntState = State.Play;
+        });
 		this.UpdateAsObservable()
 			.Where(_ => Input.GetKeyDown(KeyCode.P))
 			.Subscribe(_ => {
-				Music.Play("Music");
 				m_onPlay.OnNext(Unit.Default);
 			});
 		this.OnDestroyAsObservable()
 			.Subscribe(_ => m_onPlay.OnCompleted());
 	}
+    public void Restart() { }
+    public void SceneReset()
+    {
+        
+    }
 }
