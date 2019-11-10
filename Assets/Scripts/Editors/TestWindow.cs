0ec2422bb6c62847d8d8eba18d54072c4cd6b7e5
Yumuru <yakasa30@yahoo.co.jp>
2019-11-10 12:32:33 +0900

    Debug Window

--- Assets/Scripts/Editors/TestWindow.cs
+++ Assets/Scripts/Editors/TestWindow.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UniRx;
 
 public class TestWindow : EditorWindow {
     [MenuItem("Custom/TestWindow")]
@@ -10,6 +11,16 @@ public class TestWindow : EditorWindow {
     }
 
     void OnGUI() {
+        if (GUILayout.Button("Start")) {
+            GameManager.Ins.m_onPlay.OnNext(Unit.Default);
+        }
+
+        if (GUILayout.Button("SpawnLeft")) {
+            GameManager.Ins.m_enemyGhostManager.SpawnGhost(0);
+        }
+        if (GUILayout.Button("SpawnCenter")) {
+            GameManager.Ins.m_enemyGhostManager.SpawnGhost(1);
+        }
         if (GUILayout.Button("SpawnRight")) {
             GameManager.Ins.m_enemyGhostManager.SpawnGhost(2);
         }
