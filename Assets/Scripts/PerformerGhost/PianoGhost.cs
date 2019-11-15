11c230d34dc68d01f15c8ebd1ec9f572f9865155
森口大雅 <gucchi512@outlook.jp>
2019-11-16 01:33:14 +0900

    ピアノ幽霊編集

---
+++ Assets/Scripts/PerformerGhost/PianoGhost.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PianoGhost : MonoBehaviour
+{
+    public PerformerGhost m_performerGhost;
+
+    private void Start() {
+        m_performerGhost.Initialize(gameObject);
+        m_performerGhost.Play();
+    }
+}
